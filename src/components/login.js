52eeac951407489802a14dbbfa443f00db7ccbd2
ani <subhradeeppal30@gmail.com>
2021-12-19 18:01:03 +0530

    final

--- src/components/login.js
+++ src/components/login.js
@@ -1,44 +1,50 @@
-import React from 'react'
-import "./log-in.css"
+import React from "react";
+import "./log-in.css";
 
 const Login = () => {
-    return (
-        <>
-            
-            <section class="section">
-    <div class="container">
-        <div class="row orange">
-            <div class="flexcol-1">
-                <img src="https://source.unsplash.com/720x930/?social"  alt="Oops"/>
-            </div>
-            <div class="flexcol-2 headings" >
-                <h1 style="font-weight: bold">IDEA</h1>
-                <h4>Sign into your account</h4>
-                <form>
-                    <div>
-                        <div class="flexcol-2">
-                            <input type="text" class="form" placeholder="Username"/>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="flexcol-2">
-                            <input type="password" class="form" placeholder="******"/>
-                        </div>
-                    </div>  
-                    <div>
-                        <div class="flexcol-2">
-                           <button type="submit" class="btn1" >Login</button>
-                        </div>
-                    </div> 
-                    <a href="#">Forgot password</a>
-                    <p>Don't have an account?<a href="#">Register here</a></p>
-                </form>
-            </div>
-        </div>
-    </div>
-</section>
-        </>
-    )
-}
+	return (
+		<>
+			<section class="section">
+				<div class="container">
+					<div class="row orange">
+						<div class="flexcol-1">
+							<img
+								src="https://source.unsplash.com/720x930/?social"
+								alt="Oops"
+							/>
+						</div>
+						<div class="flexcol-2 headings">
+							<h1>IDEA</h1>
+							<h4>Sign into your account</h4>
+							<form>
+								<div>
+									<div class="flexcol-2">
+										<input type="text" class="form" placeholder="Username" />
+									</div>
+								</div>
+								<div>
+									<div class="flexcol-2">
+										<input type="password" class="form" placeholder="******" />
+									</div>
+								</div>
+								<div>
+									<div class="flexcol-2">
+										<button type="submit" class="btn1">
+											Login
+										</button>
+									</div>
+								</div>
+								<a href="#">Forgot password</a>
+								<p>
+									Don't have an account?<a href="#">Register here</a>
+								</p>
+							</form>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
 
-export default Login
+export default Login;
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,52 +1,122 @@
-import React from 'react'
-import "./log-in.css"
+import axios from "axios";
+import React from "react";
+import "./log-in.css";
+import $ from "jquery";
 const Signup = () => {
-    return (
-        <>
-          <section class=" section">
-    <div class=" container">
-        <div class="row orange">
-            <div class="flexcol-1">
-                <img src="	https://source.unsplash.com/720x1050/?social"  alt="Oops"/>
-            </div>
-            <div class=" flexcol-2 headings2 " >
-                <h1 style="font-weight: bold;">IDEA</h1>
-                <h4>Create your account</h4>
-                <form>
-                    <div>
- 
-                            <input type="email" class=" form " placeholder="Email address"/>
-                      
-                    </div>
-                    <div>
-                        
-                            <input type="text" class="form " placeholder="Username"/>
-                     
-                    </div>
-                    <div>
-                        
-                            <input type="password" class=" form " placeholder="Password"/>
-                     
-                    </div>  
-                    <div>
-                     
-                            <input type="password" class=" form " placeholder="Confirm password"/>
-                      
-                    </div>  
-                    <div>
-                        
-                           <button type="submit" class="btn2 h4" >Create account</button>
-                  
-                    </div> 
-                    <a class="small" href="#">Forgot password</a>
-                    <p class="small">Don't have an account?<a class="small" href="#">Register here</a></p>
-                </form>
-            </div>
-        </div>
-    </div>
-</section>  
-        </>
-    )
-}
+	function clickmethod() {
+		// var firstName = $("#firstName").val();
+		// var lastName = $("#lastName").val();
+		// var email = $("#email").val();
+		// var location = $("#location").val();
+		// var department = $("#department").val();
+		// var designation = $("#designation").val();
+		// var role = $("#role").val();
+		var userName = $("#username").val();
+		var password = $("#password").val();
+		var email = $("#email").val();
+		var data_uri =
+			"{" +
+			'"email":"' +
+			email +
+			'",' +
+			'"username":"' +
+			userName +
+			'",' +
+			'"password":"' +
+			password +
+			'"' +
+			"}";
+		console.log(data_uri);
+		$.ajax({
+			url: "http://127.0.0.1:8000/user/",
+			type: "POST", // http method
+			data: data_uri, // data to submit
+			//headers: {  'Access-Control-Allow-Origin': '*'},
+			crossDomain: true,
+			//dataType: 'jsonp',
+			contentType: "application/json; charset=utf-8",
+			success: function (data, status, xhr) {
+				//alert(data);
+				alert(status);
+				//$('p').append('status: ' + status + ', data: ' + data);
+			},
+			error: function (jqXhr, textStatus, errorMessage) {
+				alert(errorMessage);
+				//$('p').append('Error' + errorMessage);
+			},
+		});
+	}
 
-export default Signup
+	return (
+		<>
+			<section class=" section">
+				<div class=" container">
+					<div class="row orange">
+						<div class="flexcol-1">
+							<img
+								src="	https://source.unsplash.com/720x1050/?social"
+								alt="Oops"
+							/>
+						</div>
+						<div class=" flexcol-2 headings2 ">
+							<h1>IDEA</h1>
+							<h4>Create your account</h4>
+							<form action="http://127.0.0.1:8000/user/" method="post">
+								<div>
+									<input
+										id="email"
+										name="email"
+										type="email"
+										class=" form "
+										placeholder="Email address"
+									/>
+								</div>
+								<div>
+									<input
+										name="username"
+										id="username"
+										type="text"
+										class="form "
+										placeholder="Username"
+									/>
+								</div>
+								<div>
+									<input
+										name="password"
+										id="password"
+										type="password"
+										class=" form "
+										placeholder="Password"
+									/>
+								</div>
+								<div>
+									<input
+										type="password"
+										class=" form "
+										placeholder="Confirm password"
+									/>
+								</div>
+								<div>
+									<button type="submit" class="btn2 h4">
+										Create account
+									</button>
+								</div>
+							</form>
+							<a class="small" href="#">
+								Forgot password
+							</a>
+							<p class="small">
+								Don't have an account?
+								<a class="small" href="#">
+									Register here
+								</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default Signup;
