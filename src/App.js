f685a7afd8ea1359acd344b1b812356aa858f6f4
Astroidworld <astroidworld@gmail.com>
2021-12-21 18:52:03 +0530

    small changes 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import React from 'react'
-import Mainindex from './components/mainindex';
-import Login from './components/login';
-
-=======
 import React from "react";
 import Mainindex from "./components/mainindex";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/login";
 import Signup from "./components/signup";
->>>>>>> 9e1550ffe63ad5b6d22d3e8713b51764d352f590
 function App() {
 	return (
 		<>
